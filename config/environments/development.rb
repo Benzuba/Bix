a9e27f408471618e813507e5621a7415e89df447
Benzuba11 <zubes33@gmail.com>
2017-03-08 12:35:25 -0600

    d

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+ # Amazon Web Services - S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'consignnyc',
+      :s3_credentials => "#{Rails.root}/config/aws.yml",
+    }
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
